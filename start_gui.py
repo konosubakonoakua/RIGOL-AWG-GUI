4576bd19d58811b62090e4ba4773ea909418d8f0
konosubakonoakua <ailike_meow@qq.com>
2025-10-04 19:19:04 +0800

    refactor(format): format & rename

--- start_gui.py
+++ start_gui.py
@@ -1,11 +1,10 @@
 import sys
 import pyvisa_py
 from PyQt5.QtWidgets import QApplication
-from rigol_gui.rigol_gui import MainWindow
+from rigol_awg_gui.rigol_awg_gui import MainWindow
 
 
 app = QApplication(sys.argv)
 main_win = MainWindow()
 main_win.show()
 app.exec()
-
